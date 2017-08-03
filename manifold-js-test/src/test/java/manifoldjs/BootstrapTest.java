bd7ddca02945260fff852afe4919f6f866656a3f
smckinney <rsmckinney@hotmail.com>
2017-08-02 20:50:20 -0700

    converge java 9 changes with compiler plugin and removal of annotation processor hook

--- manifold-js-test/src/test/java/manifoldjs/BootstrapTest.java
+++ manifold-js-test/src/test/java/manifoldjs/BootstrapTest.java
@@ -11,8 +11,8 @@ public class BootstrapTest {
   @Test
   public void primordialProgramTest() {
     assertEquals("Yay", BasicJavascriptProgram.bar());
-    assertEquals(1, BasicJavascriptProgram.incrementAndGet());
-    assertEquals(2, BasicJavascriptProgram.incrementAndGet());
+    assertEquals(1.0, BasicJavascriptProgram.incrementAndGet());
+    assertEquals(2.0, BasicJavascriptProgram.incrementAndGet());
     assertEquals("Foo", BasicJavascriptProgram.identity("Foo"));
     assertEquals(1, BasicJavascriptProgram.identity(1));
   }
@@ -22,14 +22,14 @@ public class BootstrapTest {
 
     BasicJavascriptClass instance1 = new BasicJavascriptClass();
     assertEquals("Hello World", instance1.returnStr());
-    assertEquals(1, instance1.incrementAndGet());
-    assertEquals(2, instance1.incrementAndGet());
+    assertEquals(1.0, instance1.incrementAndGet());
+    assertEquals(2.0, instance1.incrementAndGet());
     assertEquals("Foo", instance1.identity("Foo"));
     assertEquals(1, instance1.identity(1));
 
     BasicJavascriptClass instance2 = new BasicJavascriptClass();
-    assertEquals(1, instance2.incrementAndGet());
-    assertEquals(2, instance2.incrementAndGet());
+    assertEquals(1.0, instance2.incrementAndGet());
+    assertEquals(2.0, instance2.incrementAndGet());
 
   }
 
