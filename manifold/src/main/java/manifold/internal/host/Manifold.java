0a1fae8c385dced0d702af0a10f055958480446a
Kyle Moore <kyle@manifold.systems>
2017-10-17 16:54:26 -0700

    Adequately purge JRE libs from sourcepath

--- manifold/src/main/java/manifold/internal/host/Manifold.java
+++ manifold/src/main/java/manifold/internal/host/Manifold.java
@@ -121,8 +121,36 @@ public class Manifold
   {
     warnIfRoot( p );
     String path = p.replace( File.separatorChar, '/' ).toLowerCase();
-    return path.contains( "/java/" ) ||
+    return isJrePath(path) ||
            path.contains( "/idea_rt.jar" );
+  }
+
+  private static boolean isJrePath( String path ) 
+  {
+    if( path.endsWith( "tools.jar" ) )
+    {
+      return true;
+    }
+
+    String extDirs = System.getProperty("java.ext.dirs");
+    if( extDirs != null && extDirs.contains( path ) )
+    {
+      return true;
+    }
+    
+    String bootPath = System.getProperty("sun.boot.class.path");
+    if( bootPath != null && bootPath.contains( path ) )
+    {
+      return true;
+    }
+    
+    String javaHome = System.getProperty("java.home");
+    if( javaHome == null )
+    {
+      return false;
+    }
+    
+    return path.startsWith( javaHome );
   }
 
   private static void warnIfRoot( String p )
