0ce81ab064f905b7007d9f798a4a2cc63af10a38
Kyle Moore <kyle@manifold.systems>
2017-10-20 11:02:48 -0700

    Merge manifold.api.type.ITypeManifold entries in manifold-all

---
+++ buildSrc/src/main/groovy/FileMergeTask.groovy
@@ -0,0 +1,23 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+import java.nio.charset.StandardCharsets
+
+class FileMergeTask extends DefaultTask {
+
+    @InputFiles
+    FileCollection inputFiles
+
+    @OutputFile
+    File target
+
+    @TaskAction
+    void mergeFiles() {
+        Set<String> lines = new HashSet<>()
+        inputFiles.each { lines.addAll(it.readLines()) }
+        target.write(String.join('\n', lines), StandardCharsets.UTF_8.name())
+    }
+}
