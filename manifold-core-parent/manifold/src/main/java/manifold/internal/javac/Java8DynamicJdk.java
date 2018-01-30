f8898010aaad587f646157102180b24e77e40365
scott <rsmckinney@hotmail.com>
2018-01-29 21:20:34 -0800

    Manifold core change - Darkj files used in core internals (while an interesting experiment) puts too much pressure on manifold to handle corner cases that otherwise don't exist e.g., separation of user sources from internal sources, performance issues from compiling and loading classes during compilation, etc. - Convert usage of darkj files used in core internals to compiled java9 classes - Compiling the Java 9 files is a one-off process for now, see the README.TXT for build instructions

---
+++ manifold-core-parent/manifold/src/main/java/manifold/internal/javac/Java8DynamicJdk.java
@@ -0,0 +1,74 @@
+package manifold.internal.javac;
+
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.model.JavacElements;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Filter;
+import com.sun.tools.javac.util.Log;
+import com.sun.tools.javac.util.Name;
+import java.util.Locale;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+
+
+public class Java8DynamicJdk implements IDynamicJdk
+{
+  @Override
+  public <T> void report( Log issueLogger, Diagnostic<? extends T> diagnostic )
+  {
+    // Adapted from JavacMessager.printMessage.  Following same basic routine regarding use of Log
+
+    JavaFileObject oldSource = issueLogger.useSource( (JavaFileObject)diagnostic.getSource() );
+    boolean oldMultipleErrors = issueLogger.multipleErrors;
+    issueLogger.multipleErrors = true;
+    try
+    {
+      switch( diagnostic.getKind() )
+      {
+        case ERROR:
+          issueLogger.error( new IssueReporter.Position( diagnostic ), "proc.messager", diagnostic.getMessage( Locale.getDefault() ) );
+          break;
+        case WARNING:
+          issueLogger.warning( new IssueReporter.Position( diagnostic ), "proc.messager", diagnostic.getMessage( Locale.getDefault() ) );
+          break;
+        case MANDATORY_WARNING:
+          issueLogger.mandatoryWarning( new IssueReporter.Position( diagnostic ), "proc.messager", diagnostic.getMessage( Locale.getDefault() ) );
+          break;
+        case NOTE:
+        case OTHER:
+          issueLogger.note( new IssueReporter.Position( diagnostic ), "proc.messager", diagnostic.getMessage( Locale.getDefault() ) );
+          break;
+      }
+    }
+    finally
+    {
+      issueLogger.useSource( oldSource );
+      issueLogger.multipleErrors = oldMultipleErrors;
+    }
+  }
+
+  @Override
+  public Iterable<Symbol> getMembers( Symbol.ClassSymbol classSym )
+  {
+    return classSym.members().getElements();
+  }
+
+  @Override
+  public Iterable<Symbol> getMembers( Symbol.ClassSymbol classSym, Filter<Symbol> filter )
+  {
+    return classSym.members().getElements( filter );
+  }
+
+  @Override
+  public Iterable<Symbol> getMembersByName( Symbol.ClassSymbol classSym, Name call )
+  {
+    return classSym.members().getElementsByName( call );
+  }
+
+  @Override
+  public Symbol.ClassSymbol getTypeElement( Context ctx, JCTree.JCCompilationUnit ignore, String fqn )
+  {
+    return JavacElements.instance( ctx ).getTypeElement( fqn );
+  }
+}
