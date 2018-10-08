38d18a5302beb3a743a01ec2161830eabb59a03c
scott <rsmckinney@hotmail.com>
2018-10-08 13:04:22 -0700

    fix test

--- manifold-deps-parent/manifold-test/src/test/java/manifold/internal/host/DefaultSingleModuleTest.java
+++ manifold-deps-parent/manifold-test/src/test/java/manifold/internal/host/DefaultSingleModuleTest.java
@@ -3,6 +3,7 @@ package manifold.internal.host;
 import junit.framework.TestCase;
 import manifold.api.gen.SrcClass;
 import manifold.internal.javac.ClassSymbols;
+import manifold.internal.runtime.Bootstrap;
 
 /**
  */
@@ -10,8 +11,11 @@ public class DefaultSingleModuleTest extends TestCase
 {
   public void testMakeSrcClassStub()
   {
+    Bootstrap.init();
     SrcClass srcClass = ClassSymbols.instance( RuntimeManifoldHost.get().getSingleModule() ).makeSrcClassStub( "java.lang.String" );
     StringBuilder sb = new StringBuilder();
     srcClass.render( sb, 0 );
+    assertTrue( sb.indexOf( "package java.lang;" ) >= 0 );
+    assertTrue( sb.indexOf( "public final class String " ) > 0 );
   }
 }
